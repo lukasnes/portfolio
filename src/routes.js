b401c7d6095e3dac3f4c07cf22133b44a2824ac3
unknown <lukasnesittka25@gmail.com>
2022-11-02 10:20:01 -0500

    route change

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,11 @@ import Home from './pages/Home'
 
 export default (
     <Routes>
-        <Route exact path="/" element={ <Home /> }/>
-        <Route exact path="/about" />
-        <Route exact path="/skills" />
-        <Route exact path="/projects" />
-        <Route exact path="/contact" />
+        <Route exact path="/portfolio/" element={ <Home /> }/>
+        <Route exact path="/portfolio/about" />
+        <Route exact path="/portfolio/skills" />
+        <Route exact path="/portfolio/projects" />
+        <Route exact path="/portfolio/contact" />
         <Route path="/" element={ <Home /> }/>
     </Routes>
 )

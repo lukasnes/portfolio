197fc3ea82d791bf69907f44618ca3c4310c3bff
unknown <lukasnesittka25@gmail.com>
2022-11-02 09:45:13 -0500

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import routes from '../routes';
+import './Main.css'
+
+const Main = () => {
+    return (
+        <main className='main'>
+            { routes }
+        </main>
+    )
+}
+
+export default Main
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+
+export default (
+    <Routes>
+        <Route exact path="/" element={ <Home /> }/>
+        <Route exact path="/about" />
+        <Route exact path="/skills" />
+        <Route exact path="/projects" />
+        <Route exact path="/contact" />
+        <Route path="/" element={ <Home /> }/>
+    </Routes>
+)
